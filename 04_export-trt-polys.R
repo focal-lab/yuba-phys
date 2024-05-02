e655da515cea4936e91d6474c76e944abe42ef3f
Derek Young <22568494+youngdjn@users.noreply.github.com>
2024-05-01 21:44:46 -0700

    Simplify FACTS polygons for writing as KML for Avenza

---
+++ 04_export-trt-polys.R
@@ -0,0 +1,29 @@
+# Purpose: Aggregate and summarize geospatial data into a format that is convenient for defining plot selection.
+# Input data includes past treatments, planned future treatments, topography (elev, SRI, etc), project area bounds
+
+## SETUP
+
+# Load packages
+
+library(terra)
+library(sf)
+library(dplyr)
+library(ggplot2)
+
+# Specify the root of the data directory
+
+DATADIR = "/home/derek/Documents/repo-data-local/yuba-phys_data/"
+
+## LOAD DATA (including some reprojection and initial visualization)
+
+# FACTS past treatment data (created by script "01_facts-data-carpentry.R")
+facts = st_read(file.path(DATADIR, "facts", "processed", "facts_thinning_focal.gpkg"))
+
+
+
+# Simplyfy the FACTS polygons as much as possible
+f1 = st_transform(facts, 3310)
+f2 = st_buffer(f1, dist = 30)
+f3 = st_union(f2)
+f4 = st_simplify(f3, dTolerance = 10)
+st_write(f4, dsn = file.path(DATADIR, "tmp", "facts_simplified.kml"), delete_dsn = TRUE)   
