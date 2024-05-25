6ec531decce17afa7420d19e4a120d3eeb8d787e
Anjum Gujral <akgujral@ucdavis.edu>
2024-05-24 22:22:57 -0700

    exporting planned treatments to kml

--- 04_export-trt-polys.R
+++ 04_export-trt-polys.R
@@ -11,19 +11,22 @@ library(dplyr)
 library(ggplot2)
 
 # Specify the root of the data directory
-
-DATADIR = "/home/derek/Documents/repo-data-local/yuba-phys_data/"
+DATADIR = "/Users/anjumgujral/Box Sync/yuba-phys_data"
+#DATADIR = "/home/derek/Documents/repo-data-local/yuba-phys_data/"
 
 ## LOAD DATA (including some reprojection and initial visualization)
 
 # FACTS past treatment data (created by script "01_facts-data-carpentry.R")
 facts = st_read(file.path(DATADIR, "facts", "processed", "facts_thinning_focal.gpkg"))
+planned = st_read(file.path(DATADIR, "future-treatment-polys", "TNC_DataRequest_Mar15.2024.gdb"))
+
+planned = st_layers(file.path(DATADIR, "future-treatment-polys", "TNC_DataRequest_Mar15.2024.gdb"))
 
 
 
 # Simplyfy the FACTS polygons as much as possible
-f1 = st_transform(facts, 3310)
+f1 = st_transform(planned, 3310)
 f2 = st_buffer(f1, dist = 30)
 f3 = st_union(f2)
 f4 = st_simplify(f3, dTolerance = 10)
-st_write(f4, dsn = file.path(DATADIR, "tmp", "facts_simplified.kml"), delete_dsn = TRUE)   
+st_write(f4, dsn = file.path(DATADIR, "tmp", "planned_simplified.kml"), delete_dsn = TRUE)   
