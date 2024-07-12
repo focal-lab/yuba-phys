11286f487279d189876b9f6125896f6b48fa49ac
Anjum Gujral <akgujral@ucdavis.edu>
2024-07-12 10:36:44 -0700

    Preliminary data check. Are plots similar in DBH and stand density?

--- 05_data-exploration.R
+++ 05_data-exploration.R
@@ -1,3 +1,11 @@
 DATADIR = "/Users/anjumgujral/Box Sync/yuba-phys_data/traits"
 trait_data <- read.csv("/Users/anjumgujral/Box Sync/yuba-phys_data/traits/yuba-phys-data.csv")
 hist(trait_data$stand_basal_area)
+plot(trait_data$height_m~trait_data$DBH_cm)
+
+install.packages('GGally')
+library('GGally')
+ggpairs(trait_data, columns = 1:14)
+boxplot(trait_data$PAR_μmol~trait_data$plot)
+
+
