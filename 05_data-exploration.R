5b966585bd87a6e6fce828ea7ecc529d79c9e4ba
Anjum Gujral <akgujral@ucdavis.edu>
2024-07-12 10:35:11 -0700

    Preliminary data check. Are plots similar in DBH and stand density?

---
+++ 05_data-exploration.R
@@ -0,0 +1,3 @@
+DATADIR = "/Users/anjumgujral/Box Sync/yuba-phys_data/traits"
+trait_data <- read.csv("/Users/anjumgujral/Box Sync/yuba-phys_data/traits/yuba-phys-data.csv")
+hist(trait_data$stand_basal_area)
