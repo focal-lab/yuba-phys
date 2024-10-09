1145cc7b9439488312485570d91fc67b8f537f1c
Anjum Gujral <akgujral@ucdavis.edu>
2024-10-09 04:03:15 -0700

    processing vulnerability curves

--- 05_data-exploration.R
+++ 05_data-exploration.R
@@ -1,11 +1,41 @@
 DATADIR = "/Users/anjumgujral/Box Sync/yuba-phys_data/traits"
 trait_data <- read.csv("/Users/anjumgujral/Box Sync/yuba-phys_data/traits/yuba-phys-data.csv")
-hist(trait_data$stand_basal_area)
-plot(trait_data$height_m~trait_data$DBH_cm)
 
-install.packages('GGally')
-library('GGally')
-ggpairs(trait_data, columns = 1:14)
-boxplot(trait_data$PAR_μmol~trait_data$plot)
+library('dplyr')
+library('tidyverse')
+
+### subset dataframe to include only seedlings and saplings for ABCO, PIPO, PILA, and ABMA
+trait_data1 <- trait_data[trait_data$size_class %in% c('seedling', 'sapling'), ]
+trait_data1 <- trait_data[trait_data$size_class == 'seedling', 'sapling',]
+trait_data1 <- trait_data1[trait_data1$species %in% c('ABCO', 'ABMA','PIPO', 'PILA'), ]
+  
+### summary statistics of dataframe
+
+### make a table for counts of seedlings and saplings per species
+counts_table <- table(trait_data1$species[trait_data1$species %in% c('ABCO', 'ABMA','PIPO','PILA')],
+                     trait_data1$size_class[trait_data1$size_class %in% c('seedling', 'sapling')])
+
+# make a new column turning canopy closure into a categorical variable
+trait_data1$microsite <- cut(
+  trait_data1$percent_cover,
+  breaks = c(0, 68, 85, 100),
+  labels = c('sun', 'int', 'shade'),
+  right = TRUE
+)
+
+## make a table for counts of seedlings and saplings per species by elevation and microsite
+summary_table <- trait_data1 %>%
+  filter(size_class %in% c('seedling', 'sapling'), 
+         species %in% c('ABCO','ABMA','PIPO','PILA')) %>%
+  group_by(species, size_class, elevation, microsite) %>%
+  summarize(count = n(), .groups = 'drop')
+
+## counts by elevation
+low_elev_counts <- summary_table %>%
+  filter(elevation %in% c('low'))
+
+high_elev_counts <- summary_table %>%
+  filter(elevation %in% c('high'))
+
 
 
